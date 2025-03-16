d1ed63589b809247008bcac42f7fda29d25dc32f
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2025-03-16 06:16:56 -0300

    fix: removing db name, now is a env config

--- memory_management/models.py
+++ memory_management/models.py
@@ -3,8 +3,15 @@ from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.postgresql import JSONB
 import datetime
+import os
+from dotenv import load_dotenv
+from pathlib import Path
 
-DATABASE_URL = "postgresql://matheus:password@localhost/users_chat_history"
+root = Path(__file__).parent.parent
+load_dotenv(root / ".env")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
